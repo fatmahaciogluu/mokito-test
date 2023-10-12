17c081deba6c38948b7f273af69a548b6aedc8e1
fhacioglu <fatma.368tn@gmail.com>
2023-10-12 10:59:28 +0300

    controller-test added

---
+++ src/test/java/com/fhaciioglu/controllertesting/ControllertestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhaciioglu.controllertesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllertestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
