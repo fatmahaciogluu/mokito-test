611bf605bb90c2b492b577825fd3653cc767952f
fhacioglu <fatma.368tn@gmail.com>
2023-10-12 11:06:55 +0300

    Controller-Test pages added

--- src/main/java/com/fhaciioglu/controllertesting/service/WelcomeService.java
+++ src/main/java/com/fhaciioglu/controllertesting/service/WelcomeService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class WelcomeService {
     public String getWelcomeMessage(String name) {
+
         return String.format("Welcome %s!", name);
     }
 }
